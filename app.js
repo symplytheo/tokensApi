905907b8f00818aad98c3aeb607be47ead1db990
Theo Ojochegbe <symplytheo@gmail.com>
2022-03-30 20:22:23 +0100

    feat: init, configure server entry file

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+require('dotenv/config');
+const { connectDatabase } = require('./config/db');
+const { tokenRoutes } = require('./routes/tokenRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDatabase();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// token routes
+app.use('/tokens', tokenRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello mon API');
+});
+
+app.listen(PORT, () => {
+  console.log(`[Server]: Listening on port ${PORT}`);
+});
